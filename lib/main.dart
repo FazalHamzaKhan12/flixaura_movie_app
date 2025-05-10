9c3e0f44316a91e21c63d63f72faad4830d759af
Fazal Hamza Khan <theunknown.pak@gmail.com>
2025-05-10 09:32:16 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flixaura_movie_app/start_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: StartScreen()w);
+  }
+}
